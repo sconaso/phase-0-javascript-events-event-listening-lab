387d0cb53eff47609aba1cb69513095d440f5124
Scott Nason <scottmnason@gmail.com>
2022-05-25 22:46:12 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
-    input.addEventListener('click', function() {
-        alert('I was clicked!');
-    })
+    const input = document.getElementById('input');
+    input.addEventListener('click', () => alert('I was clicked!'));
 }
+
+addingEventListener();
